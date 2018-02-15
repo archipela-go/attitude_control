6139e2222d27f2a59e7d23dee9f1b9857c123c59
Anurag Makineni <anurag.makineni@gmail.com>
2018-02-14 23:21:41 -0500

    subscriber working

--- attitude_control/src/attitude_control_node.cpp
+++ attitude_control/src/attitude_control_node.cpp
@@ -1,9 +1,17 @@
 #include <ros/ros.h>
 #include <mavros_msgs/AttitudeTarget.h>
 
+void setpoint_cb(const mavros_msgs::AttitudeTarget::ConstPtr &msg)
+{
+  ROS_INFO("setpoint recieved");
+}
+
 int main(int argc, char** argv) {
     ros::init(argc, argv, "attitude_control_node");
     ros::NodeHandle pnh("~");
+
+    ros::Subscriber setpoint_sub = pnh.subscribe("attitude_target", 1000, setpoint_cb);
+
     ros::spin();
     return 0;
 }
