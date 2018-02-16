c1d25b8c05527bce8ef05854c71de0c20fbc7b2c
Anurag Makineni <anurag.makineni@gmail.com>
2018-02-16 11:52:55 -0500

    save imu orientation as quaternion

--- attitude_control/src/attitude_controller.cpp
+++ attitude_control/src/attitude_controller.cpp
@@ -15,21 +15,25 @@ class Node {
 
  private:
   ros::NodeHandle pnh_;
-  ros::Subscriber setpoint_sub;
+
+  ros::Subscriber setpoint_sub_;
   ros::Time last_setpoint_time_;
   mavros_msgs::AttitudeTarget last_setpoint_;
-  ros::Subscriber imu_sub;
+
+  ros::Subscriber imu_sub_;
+  Eigen::Quaterniond imu_q_;
 };
 
 Node::Node(const ros::NodeHandle& pnh) : pnh_(pnh) {
-  setpoint_sub = pnh_.subscribe("/attitude_target", 10, &Node::setpoint_cb, this);
-  imu_sub = pnh_.subscribe("/imu/data", 10, &Node::imu_cb, this);
+  setpoint_sub_ = pnh_.subscribe("/attitude_target", 10, &Node::setpoint_cb, this);
+  imu_sub_ = pnh_.subscribe("/imu/data", 10, &Node::imu_cb, this);
   ROS_INFO("init attitude_controller");
 }
 
 void Node::imu_cb(const sensor_msgs::Imu::ConstPtr &msg)
 {
   ROS_INFO("imu recieved");
+  imu_q_ = Eigen::Quaterniond(msg->orientation.w, msg->orientation.x, msg->orientation.y, msg->orientation.z);
 }
 
 void Node::setpoint_cb(const mavros_msgs::AttitudeTarget::ConstPtr &msg)
